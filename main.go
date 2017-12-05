004ec7505ba2f8bfd1a49b1607edb8d866d4362a
Marcos César de Oliveira <mcesarhm@gmail.com>
2017-12-05 10:42:06 -0200

    Adapt to new config and apisupport format

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -18,10 +17,6 @@ import (
 
 var api *apisupport.Api
 
-var datasourceSettings map[string]interface{}
-
-var newDataSource func(map[string]interface{}, *string, *string) (interface{}, error)
-
 type reportgenerator struct {
 	*financialstatements.ReportGenerator
 	user  string
@@ -31,7 +26,7 @@ type reportgenerator struct {
 var reportgeneratorPool = sync.Pool{
 	New: func() interface{} {
 		r := &reportgenerator{}
-		v, err := newDataSource(datasourceSettings, &r.user, &r.coaid)
+		v, err := api.Config().Run("NewFinancialStatementsDataSource", &r.user, &r.coaid)
 		if err != nil {
 			panic(err)
 		}
@@ -40,34 +35,27 @@ var reportgeneratorPool = sync.Pool{
 	},
 }
 
-type decoder func(interface{}) error
-
 func handler(
-	f func(*reportgenerator, httprouter.Params, url.Values, decoder) (interface{}, error),
+	f func(*reportgenerator, httprouter.Params, url.Values, apisupport.Decoder) (interface{}, error),
 ) func(http.ResponseWriter, *http.Request, httprouter.Params) {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		user, err := api.UserFromRequest(r)
-		if api.Check(err, w) {
+		user := api.UserFromRequest(w, r)
+		if user == "" {
 			return
 		}
 		rg := reportgeneratorPool.Get().(*reportgenerator)
 		rg.user = user
 		rg.coaid = ps.ByName("coa")
 		defer reportgeneratorPool.Put(rg)
-		v, err := f(rg, ps, r.URL.Query(), func(v interface{}) error {
-			return json.NewDecoder(r.Body).Decode(v)
+		api.Run(w, func() (interface{}, error) {
+			return f(rg, ps, r.URL.Query(), func(v interface{}) error {
+				return api.Decode(r, v)
+			})
 		})
-		if api.Check(err, w) {
-			return
-		}
-		if v != nil {
-			w.Header().Set("Content-Type", "application/json")
-			api.Check(json.NewEncoder(w).Encode(v), w)
-		}
 	}
 }
 
-func balance(rg *reportgenerator, ps httprouter.Params, uv url.Values, d decoder) (interface{}, error) {
+func balance(rg *reportgenerator, ps httprouter.Params, uv url.Values, d apisupport.Decoder) (interface{}, error) {
 	var at time.Time
 	if err := parse(uv.Get("at")).into(&at); err != nil {
 		return nil, err
@@ -75,7 +63,7 @@ func balance(rg *reportgenerator, ps httprouter.Params, uv url.Values, d decoder
 	return rg.BalanceSheet(at)
 }
 
-func incomeStatement(rg *reportgenerator, ps httprouter.Params, uv url.Values, d decoder) (interface{}, error) {
+func incomeStatement(rg *reportgenerator, ps httprouter.Params, uv url.Values, d apisupport.Decoder) (interface{}, error) {
 	var from, to time.Time
 	if err := parse(uv.Get("from"), uv.Get("to")).into(&from, &to); err != nil {
 		return nil, err
@@ -83,7 +71,7 @@ func incomeStatement(rg *reportgenerator, ps httprouter.Params, uv url.Values, d
 	return rg.IncomeStatement(from, to)
 }
 
-func journal(rg *reportgenerator, ps httprouter.Params, uv url.Values, d decoder) (interface{}, error) {
+func journal(rg *reportgenerator, ps httprouter.Params, uv url.Values, d apisupport.Decoder) (interface{}, error) {
 	var from, to time.Time
 	if err := parse(uv.Get("from"), uv.Get("to")).into(&from, &to); err != nil {
 		return nil, err
@@ -91,7 +79,7 @@ func journal(rg *reportgenerator, ps httprouter.Params, uv url.Values, d decoder
 	return rg.Journal(from, to)
 }
 
-func ledger(rg *reportgenerator, ps httprouter.Params, uv url.Values, d decoder) (interface{}, error) {
+func ledger(rg *reportgenerator, ps httprouter.Params, uv url.Values, d apisupport.Decoder) (interface{}, error) {
 	var from, to time.Time
 	if err := parse(uv.Get("from"), uv.Get("to")).into(&from, &to); err != nil {
 		return nil, err
@@ -117,35 +105,11 @@ func main() {
 		fmt.Printf("usage: %v settings", path.Base(os.Args[0]))
 		return
 	}
-	api = apisupport.NewApi()
-	var settings struct {
-		FinancialStatements struct {
-			PluginFile string                 `yaml:"PluginFile"`
-			Settings   map[string]interface{} `yaml:",inline"`
-		} `yaml:"FinancialStatements"`
-		OpenId struct {
-			Provider string `yaml:"Provider"`
-			ClientId string `yaml:"ClientId"`
-		} `yaml:"OpenId"`
-	}
-	err := api.UnmarshalSettings(os.Args[1], &settings)
+	var err error
+	api, err = apisupport.New(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
-	api.SetClientCredentials(settings.OpenId.Provider, settings.OpenId.ClientId)
-	if api.Error() != nil {
-		log.Fatal(api.Error())
-	}
-	symbol, err := api.LoadSymbol(settings.FinancialStatements.PluginFile, "NewDataSource")
-	if err != nil {
-		log.Fatal(err)
-	}
-	newDataSource = symbol.(func(map[string]interface{}, *string, *string) (interface{}, error))
-	symbol, err = api.LoadSymbol(settings.FinancialStatements.PluginFile, "LoadSymbolFunction")
-	if err == nil {
-		*symbol.(*func(string, string) (interface{}, error)) = api.LoadSymbol
-	}
-	datasourceSettings = settings.FinancialStatements.Settings
 	router := httprouter.New()
 	router.GET("/charts-of-accounts/:coa/balance-sheet", handler(balance))
 	router.GET("/charts-of-accounts/:coa/income-statement", handler(incomeStatement))
